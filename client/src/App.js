f77739dfee0699c31efb9961346dd98ae44e8aa6
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-10-24 12:11:41 -0700

    Final commit

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,33 @@ import Footer from './components/layout/Footer';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Register from './components/auth/Register';
-import Login from './components/auth/Login';
 import { Provider } from 'react-redux';
 import store from './store';
 import jwt_decode from 'jwt-decode';
 import setAuthToken from './utils/setAuthToken';
-import { setCurrentUser,logoutUser } from './actions/authActions';
+import { setCurrentUser, logoutUser } from './actions/authActions';
+import { clearCurrentProfile } from './actions/profileActions';
+import PrivateRoute from './components/common/PrivateRoute';
+import NotFound from './components/not-found/NotFound';
+//import Navbar from './components/layout/Navbar';
+//import Footer from './components/layout/Footer';
+//import Landing from './components/layout/Landing';
+import Register from './components/auth/Register';
+import Login from './components/auth/Login';
+import Dashboard from './components/dashboard/Dashboard';
+import CreateProfile from './components/create-profile/CreateProfile';
+import EditProfile from './components/edit-profile/EditProfile';
+import AddExperience from './components/add-credentials/AddExperience';
+import AddEducation from './components/add-credentials/AddEducation';
+import Profiles from './components/profiles/Profiles';
+import Profile from './components/profile/Profile';
 
 
-//check for token
+// check for token
 if (localStorage.jwtToken) {
     //set auth token header
   setAuthToken(localStorage.jwtToken);
+
   const decoded = jwt_decode(localStorage.jwtToken);
 //decode token and get the user
 //set user into redux
@@ -26,6 +40,8 @@ if (localStorage.jwtToken) {
   if (decoded.exp < currentTime){
     //logout user
     store.dispatch(logoutUser());
+    //Clear current Profile
+    store.dispatch(clearCurrentProfile());
     //redirect to login
     window.location.href = '/login';
 }
