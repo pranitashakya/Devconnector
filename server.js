470f06554dba50cbe6bb838ab7f15f04899037f8
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-10-25 10:37:15 -0700

    final commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 const bodyParser = require("body-parser");
 const passport = require("passport");
+const path = require("path");
 
 const app = express(); //instance of express
 
@@ -34,6 +35,13 @@ app.get("/", (req, res) => res.send("hello!"));
 app.use("/api/users", users);
 app.use("/api/profile", profile);
 app.use("/api/posts", posts);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(_dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = 5400;
 app.listen(port, () => console.log(`Server running on port ${port}`));
