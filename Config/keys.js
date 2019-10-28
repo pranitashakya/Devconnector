e1f51c41dd066f23ebf4e96ad5d651bf11c317e1
pranitashakya <52680240+Pranita2019-lab@users.noreply.github.com>
2019-10-28 12:10:48 -0700

    Final code

--- Config/keys.js
+++ Config/keys.js
@@ -1,6 +1,5 @@
-module.exports = {
-  mongoURI:
-    // "mongdb+srv://Devadmin:Password123@devconnector-svgcy.mongodb.net/test?retryWrites=true&w=majority"
-    "mongodb+srv://devadmin:mongodb2019@cluster0-0pbud.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "secret"
-};
+if (process.env.NODE_ENV === 'production') {
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
+}
